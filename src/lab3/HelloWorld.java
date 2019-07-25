bce58ad06098dca27287dbb2626ae144d32650bf
pukky <sorayapa@nvc.ac.th>
2019-07-25 16:49:22 +0700

    first upload

---
+++ src/lab3/HelloWorld.java
@@ -0,0 +1,12 @@
+package lab3;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        System .out .println("HelloWorld");
+        System .out .println("น.ส โสรญา เภามี");
+        System .out .println("รหัสนักศึกษา : 362211760014");
+        System .out .println("สาขา : การจัดการเทคโนโลยีสารสนเทศ");
+        System .out .println("E-mail : sorayapa@nvc.ac.th");
+    }
+}//class
---
+++ src/lab4/exinput.java
@@ -0,0 +1,36 @@
+package lab4;
+
+import java.util.Scanner;
+
+public class exinput {
+
+    public static  void  main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("please, enter an integer:");
+        int x = scanner.nextInt();
+
+        System.out.println("Your entered integer: "+x);
+
+        System.out.print("please, enter an Double:");
+        Double b = scanner.nextDouble();
+
+        System.out.println("Your entered Double: "+b);
+
+        Scanner scanner1 = new Scanner((System.in));
+
+        System.out.print("please, enter You name:");
+        String n = scanner.nextLine();
+
+        System.out.println("Your name is: "+n);
+
+
+
+
+
+
+
+    }
+
+}
