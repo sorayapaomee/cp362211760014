cc0e08dfdc1a4046795945e2c3144eff63c3de1c
pukky <sorayapa@nvc.ac.th>
2019-10-27 15:57:11 +0700

    lab6

---
+++ src/lab10/ex10.java
@@ -0,0 +1,12 @@
+package lab10;
+
+public class ex10 {
+    public static void main(String[] args) {
+
+        max(5,10);
+        max(5.5,10.10);
+
+    }
+    public static void max(int x,int y) {}
+    public static void max(double x, double y){}
+}
